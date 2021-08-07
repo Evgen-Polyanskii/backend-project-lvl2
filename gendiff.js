60cfb2ad789c5d9cc127ecd0d35dc727ff8bc982
Polyansky <m4x1morlov@yandex.ru>
2021-08-07 12:53:00 +0300

    Fixed gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -6,11 +6,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'Output the current version');
+  .version('v0.0.1')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'Output format');
 
-program.parse();
-
-const options = program.opts();
-if (options.version) {
-  console.log('v0.0.1');
-}
+program.parse(process.argv);
