183b11f9042d2469fb9824b00ae8b9ca80ddf7e0
Polyansky <m4x1morlov@yandex.ru>
2021-08-17 19:59:29 +0300

    Fixed tests

--- __tests__/gendiff.tests.js
+++ __tests__/gendiff.tests.js
@@ -1,8 +1,7 @@
 /* eslint operator-linebreak: 0 */
 import { fileURLToPath } from 'url';
 import path from 'path';
-import { readFileSync } from 'fs';
-import getDifference from '../src/getDifference';
+import gendiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -14,16 +13,16 @@ describe('Сhecking to get the difference between files', () => {
   test('files.json', () => {
     const diff =
       '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
-    const file1 = readFileSync(getFixturePath('file1.json'), 'utf-8');
-    const file2 = readFileSync(getFixturePath('file2.json'), 'utf-8');
-    expect(getDifference(file1, file2)).toEqual(diff);
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
+    expect(gendiff(file1, file2)).toEqual(diff);
   });
 
   test('files.yaml', () => {
     const diff =
-      '{\n- name: Andrey\n+ name: Sergey\n- age: 25\n+ age: 24\n  married: true\n  job: driver\n+ number: 55-55-55}';
-    const file1 = readFileSync(getFixturePath('file1.yml'), 'utf-8');
-    const file2 = readFileSync(getFixturePath('file2.yaml'), 'utf-8');
-    expect(getDifference(file1, file2)).toEqual(diff);
+      '{\n- age: 25\n+ age: 24\n  job: driver\n  married: true\n- name: Andrey\n+ name: Sergey\n+ number: 55-55-55\n}';
+    const file1 = getFixturePath('file1.yml');
+    const file2 = getFixturePath('file2.yaml');
+    expect(gendiff(file1, file2)).toEqual(diff);
   });
 });
