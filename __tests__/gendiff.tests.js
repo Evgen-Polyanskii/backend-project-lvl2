f8e1620278a3995002695937f9306242febed465
Polyansky <m4x1morlov@yandex.ru>
2021-08-24 20:19:26 +0300

    Changing the tests, added checks of nested structures

--- __tests__/gendiff.tests.js
+++ __tests__/gendiff.tests.js
@@ -1,4 +1,4 @@
-/* eslint operator-linebreak: 0 */
+import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import gendiff from '../index.js';
@@ -10,19 +10,17 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 describe('Сhecking to get the difference between files', () => {
-  test('files.json', () => {
-    const diff =
-      '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
-    const file1 = getFixturePath('file1.json');
-    const file2 = getFixturePath('file2.json');
+  test('Expansion .json and format stylish', () => {
+    const file1 = getFixturePath('before1.json');
+    const file2 = getFixturePath('before2.json');
+    const diff = readFileSync(getFixturePath('after1(json)'), 'utf8');
     expect(gendiff(file1, file2)).toEqual(diff);
   });
 
-  test('files.yaml', () => {
-    const diff =
-      '{\n- age: 25\n+ age: 24\n  job: driver\n  married: true\n- name: Andrey\n+ name: Sergey\n+ number: 55-55-55\n}';
-    const file1 = getFixturePath('file1.yml');
-    const file2 = getFixturePath('file2.yaml');
+  test('Expansion .yaml and format stylish', () => {
+    const file1 = getFixturePath('before1.yml');
+    const file2 = getFixturePath('before2.yaml');
+    const diff = readFileSync(getFixturePath('after1(yaml)'), 'utf8');
     expect(gendiff(file1, file2)).toEqual(diff);
   });
 });
