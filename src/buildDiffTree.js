3094c27e25d6f9300d02d02e86c3aab08e1cc59f
Polyansky <m4x1morlov@yandex.ru>
2021-08-29 18:28:05 +0300

    Refactoring

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -4,36 +4,20 @@ const buildDiffTree = (object1, object2) => {
   const keysFromFile1 = Object.keys(object1);
   const keysFromFile2 = Object.keys(object2);
   const sortKeys = _.sortBy(_.union(keysFromFile1, keysFromFile2));
-  const diffTree = sortKeys.map((key) => {
+  return sortKeys.map((key) => {
     const value1 = object1[key];
     const value2 = object2[key];
     if (!_.has(object2, key)) {
-      return {
-        key,
-        value: value1,
-        type: 'removed',
-      };
+      return { key, value: value1, type: 'removed' };
     }
     if (!_.has(object1, key)) {
-      return {
-        key,
-        value: value2,
-        type: 'added',
-      };
+      return { key, value: value2, type: 'added' };
     }
     if (_.isEqual(value1, value2)) {
-      return {
-        key,
-        value: value1,
-        type: 'unchangeable',
-      };
+      return { key, value: value1, type: 'unchangeable' };
     }
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return {
-        key,
-        type: 'nested',
-        children: buildDiffTree(value1, value2),
-      };
+      return { key, type: 'nested', children: buildDiffTree(value1, value2) };
     }
     return {
       key,
@@ -42,7 +26,6 @@ const buildDiffTree = (object1, object2) => {
       type: 'modified',
     };
   });
-  return diffTree;
 };
 
 export default buildDiffTree;
