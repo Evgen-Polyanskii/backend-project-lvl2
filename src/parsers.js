ae337363ef59a2894631ba3a5e430b7cc17c8470
Evgen-Polyanskii <77778052+Evgen-Polyanskii@users.noreply.github.com>
2021-08-31 13:21:48 +0300

    Updated parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,16 @@
+import _ from 'lodash';
 import yaml from 'js-yaml';
 
-export default (data, format) => {
-  if (format === '.yml' || format === '.yaml') {
-    const parse = yaml.load;
-    return parse(data);
+const parsers = {
+  '.yaml': (data) => yaml.load(data),
+  '.yml': (data) => yaml.load(data),
+  '.json': (data) => JSON.parse(data),
+};
+
+export default (data, extension) => {
+  if (!_.has(parsers, extension)) {
+    throw new Error(`Unknown extension: '${extension}'!`);
   }
-  return JSON.parse(data);
+  const parser = parsers[extension];
+  return parser(data);
 };
